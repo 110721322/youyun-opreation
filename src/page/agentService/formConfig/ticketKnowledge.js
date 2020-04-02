5e6dbab3584e0fa57ff4ec0e76b25833764a9833
王挺 <936988047@qq.com>
2020-04-02 15:47:06 +0800

    mod: 工单知识库开发

---
+++ src/page/agentService/formConfig/ticketKnowledge.js
@@ -0,0 +1,103 @@
+import { setRules } from '@/libs/kit/formFns.js'
+
+export const FORM_CONFIG = {
+  addQuestionData: {
+    title: '添加问题',
+    showFootBtn: true,
+    formData: [
+      {
+        type: 1,
+        label: '所属分类',
+        key: 'type',
+        initVal: "0",
+        options: [
+          {
+            label: '川菜',
+            value: '0'
+          },
+          {
+            label: '粤菜',
+            value: '1'
+          },
+          {
+            label: '杭帮菜',
+            value: '2'
+          }
+        ],
+        rules: setRules('广告名称').isRequired.get
+      },
+      {
+        type: 0,
+        label: '问题',
+        key: 'answer',
+        initVal: '',
+        inputType: 'textarea',
+        rules: setRules('问题').isRequired.get
+      },
+      {
+        type: 0,
+        label: '答案',
+        key: 'question',
+        initVal: '',
+        inputType: 'textarea',
+        rules: setRules('答案').isRequired.get
+      }
+    ]
+  },
+  menuData: {
+    title: '添加菜单',
+    showFootBtn: true,
+    formData: [
+      {
+        type: 0,
+        label: '菜单名称',
+        key: 'type',
+        initVal: "",
+        rules: setRules('菜单名称').isRequired.get
+      }
+    ]
+  },
+  editQuestionData: {
+    title: '编辑问题',
+    showFootBtn: true,
+    formData: [
+      {
+        type: 1,
+        label: '所属分类',
+        key: 'type',
+        initVal: "0",
+        options: [
+          {
+            label: '川菜',
+            value: '0'
+          },
+          {
+            label: '粤菜',
+            value: '1'
+          },
+          {
+            label: '杭帮菜',
+            value: '2'
+          }
+        ],
+        rules: setRules('广告名称').isRequired.get
+      },
+      {
+        type: 0,
+        label: '问题',
+        key: 'answer',
+        initVal: '',
+        inputType: 'textarea',
+        rules: setRules('问题').isRequired.get
+      },
+      {
+        type: 0,
+        label: '答案',
+        key: 'question',
+        initVal: '',
+        inputType: 'textarea',
+        rules: setRules('答案').isRequired.get
+      }
+    ]
+  }
+}
--- src/page/agentService/ticketCenter/ticketKnowledge.vue
+++ src/page/agentService/ticketCenter/ticketKnowledge.vue
@@ -6,34 +6,109 @@
 
     <div class="table_box">
       <div class="left-box">
-        <div class="add-first-menu">
-          <i class></i>添加一级菜单
+        <div class="add-first-menu" @click="onClick_addFirstMenu">
+          <i class="el-icon-plus el-icon--left"></i>添加一级菜单
         </div>
+        <el-tree
+          :data="treeData"
+          node-key="id"
+          default-expand-all
+          :expand-on-click-node="false"
+          class="tree-box"
+          draggable
+          :allow-drop="allowDrop"
+          @node-click="handleNodeClick"
+        >
+          <template v-slot="{ node, data }">
+            <span class="custom-tree-node">
+              <span>{{ node.label }}</span>
+              <span>
+                <el-button
+                  v-if="data.children"
+                  type="text"
+                  size="mini"
+                  @click="onClick_append(node,data)"
+                >添加</el-button>
+                <el-button type="text" size="mini" @click="onClick_delete(node,data)">删除</el-button>
+              </span>
+            </span>
+          </template>
+        </el-tree>
       </div>
-      <div class="right-box"></div>
+      <div class="right-box">
+        <div class="add-question">
+          <el-button type="primary" class="question-btn" @click="onClick_addQuestion">添加问题</el-button>
+        </div>
+        <div class="title-box">
+          <span class="title">{{ selectTreeData.label }}</span>
+          <span class="record">（共{{ questions.length }}条记录）</span>
+        </div>
+        <template v-if="questions.length!==0">
+          <div class="questions-box">
+            <div v-for="(item,index) in questions" :key="index" class="question-item">
+              <div class="question">
+                <span>
+                  <i class="el-icon-question question-icon"></i>
+                  <span class="content">{{ item.question }}</span>
+                </span>
+                <span>
+                  <span class="edit" @click="onClick_editQuestion">编辑</span>
+                  <span class="delete" @click="onClick_deleteQuestion">删除</span>
+                </span>
+              </div>
+              <div class="answer-box" v-html="item.answer"></div>
+              <div class="info-box">
+                <span>所属分类：{{ item.category }}</span>
+                <span class="time">更新时间：{{ item.createTime }}</span>
+              </div>
+            </div>
+          </div>
+          <div class="crud-pagination">
+            <el-pagination
+              size="medium"
+              :current-page="currentPage"
+              :page-sizes="[10, 20, 30, 40]"
+              :page-size="currentPageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="dataTotal"
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+            />
+          </div>
+        </template>
+        <template v-if="questions.length===0">
+          <div class="no-knowledge">
+            <img class="img" :src="ticketKnowledgeImg" alt />
+            <div class="tip">暂无相关知识</div>
+          </div>
+        </template>
+      </div>
     </div>
-
     <el-drawer :visible.sync="drawer" :with-header="false">
       <div class="p_head">{{ fromConfigData.title }}</div>
       <Form
         size="medium"
         :form-base-data="fromConfigData.formData"
         :show-foot-btn="fromConfigData.showFootBtn"
-        label-width="130px"
+        label-width="90px"
         @cancel="cancel"
       ></Form>
     </el-drawer>
   </div>
 </template>
 <script>
+import ticketKnowledgeImg from "@/assets/img/ticketKnowledge.png";
 import api from "@/api/api_params";
 import Form from "@/components/form/index.vue";
+import { FORM_CONFIG } from "../formConfig/ticketKnowledge";
 
 export default {
   name: "Theme",
   components: { Form },
   data() {
     return {
+      id: 10000,
+      ticketKnowledgeImg: ticketKnowledgeImg,
       fromConfigData: {},
       testData: [],
       drawer: false,
@@ -41,6 +116,9 @@ export default {
       ruleForm: {
         name1: "2"
       },
+      currentPage: 1,
+      currentPageSize: 10,
+      dataTotal: 0,
       isEdit: false,
       tableData: [],
       menuList: [
@@ -56,14 +134,161 @@ export default {
         }
       ],
       selectMenu: {},
-      type: "employee_edit"
+      type: "employee_edit",
+      treeData: [
+        {
+          id: 1,
+          label: "一级 1",
+          children: [
+            {
+              id: 9,
+              label: "三级 1-1-1"
+            },
+            {
+              id: 10,
+              label: "三级 1-1-2"
+            }
+          ]
+        },
+        {
+          id: 2,
+          label: "一级 2",
+          children: [
+            {
+              id: 5,
+              label: "二级 2-1"
+            },
+            {
+              id: 6,
+              label: "二级 2-2"
+            }
+          ]
+        },
+        {
+          id: 3,
+          label: "一级 3",
+          children: [
+            {
+              id: 7,
+              label: "二级 3-1"
+            },
+            {
+              id: 8,
+              label: "二级 3-2"
+            }
+          ]
+        }
+      ],
+      questions: [
+        {
+          question: "如何提现",
+          answer:
+            "a，佣金不低于500才可申请提现；\n\nb，提现时间需在本月28日-下月10日；\n\nc，交易与活动佣金可分开提现。",
+          createTime: "2020-02-20 15:22：2",
+          category: "财务类 - 佣金提现"
+        },
+        {
+          question: "如何提现",
+          answer:
+            "a，佣金不低于500才可申请提现；\n\nb，提现时间需在本月28日-下月10日；\n\nc，交易与活动佣金可分开提现。",
+          createTime: "2020-02-20 15:22：2",
+          category: "财务类 - 佣金提现"
+        },
+        {
+          question: "如何提现",
+          answer:
+            "a，佣金不低于500才可申请提现；\n\nb，提现时间需在本月28日-下月10日；\n\nc，交易与活动佣金可分开提现。",
+          createTime: "2020-02-20 15:22：2",
+          category: "财务类 - 佣金提现"
+        },
+        {
+          question: "如何提现",
+          answer:
+            "a，佣金不低于500才可申请提现；\n\nb，提现时间需在本月28日-下月10日；\n\nc，交易与活动佣金可分开提现。",
+          createTime: "2020-02-20 15:22：2",
+          category: "财务类 - 佣金提现"
+        }
+      ],
+      selectTreeData: {
+        label: "佣金提现"
+      }
     };
   },
   mounted() {
     this.selectMenu = this.menuList[0];
     this.getTableData();
+    this.transferQuestions();
   },
   methods: {
+    allowDrop(draggingNode, dropNode, type) {
+      if (draggingNode.level === dropNode.level) {
+        if (draggingNode.parent.id === dropNode.parent.id) {
+          // api请求 {}
+          return type === "prev" || type === "next";
+        }
+      } else {
+        // 不同级进行处理
+      }
+    },
+    handleNodeClick($data) {
+      console.log($data);
+    },
+    onClick_addFirstMenu() {
+      this.fromConfigData = FORM_CONFIG.menuData;
+      this.drawer = true;
+    },
+    onClick_addQuestion() {
+      this.fromConfigData = FORM_CONFIG.addQuestionData;
+      this.drawer = true;
+    },
+    onClick_editQuestion() {
+      this.fromConfigData = FORM_CONFIG.editQuestionData;
+      this.drawer = true;
+    },
+    transferQuestions() {
+      this.questions.forEach(item => {
+        item.answer = item.answer.replace(/\n/g, "<br/>");
+      });
+    },
+    onClick_append($node, $data) {
+      const newChild = { id: this.id++, label: "testtest", children: [] };
+      if (!$data.children) {
+        this.$set($data, "children", []);
+      }
+      this.data.children.push(newChild);
+    },
+    onClick_delete($node, $data) {
+      this.$confirm("是否要删除该菜单？", "提示", {
+        distinguishCancelAndClose: true,
+        confirmButtonText: "确认删除",
+        cancelButtonText: "取消"
+      })
+        .then(() => {
+          api.unfrozen({}).then(res => {
+            this.$message({
+              type: "info",
+              message: "已删除"
+            });
+          });
+        })
+        .catch(() => {});
+    },
+    onClick_deleteQuestion($node, $data) {
+      this.$confirm("是否要删除该问题？", "提示", {
+        distinguishCancelAndClose: true,
+        confirmButtonText: "确认删除",
+        cancelButtonText: "取消"
+      })
+        .then(() => {
+          api.unfrozen({}).then(res => {
+            this.$message({
+              type: "info",
+              message: "已删除"
+            });
+          });
+        })
+        .catch(() => {});
+    },
     onClick_menuItem($item) {
       this.selectMenu = $item;
       this.type = $item.type;
@@ -75,6 +300,14 @@ export default {
     search() {
       // eslint-disable-next-line no-console
       console.log(this.ruleForm);
+    },
+    handleCurrentChange(page) {
+      this.currentPage = page;
+      this.getTableData();
+    },
+    handleSizeChange(size) {
+      this.currentPageSize = size;
+      this.getTableData();
     },
     getTableData(params) {
       api
@@ -107,8 +340,8 @@ export default {
       // eslint-disable-next-line no-console
       console.log($val);
     },
-    cancel(done) {
-      done();
+    cancel() {
+      this.drawer = false;
     },
     handleClose(tag) {
       this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
@@ -147,12 +380,118 @@ export default {
     width: 34%;
     background: #ffffff;
     overflow: auto;
+    padding: 18px;
+    .add-first-menu {
+      width: 100%;
+      margin: auto;
+      height: 40px;
+      line-height: 40px;
+      background: rgba(243, 243, 243, 1);
+      color: rgba(96, 98, 102, 1);
+      text-align: center;
+      border-radius: 4px;
+      font-size: 14px;
+      border: 1px solid rgba(220, 223, 230, 1);
+    }
+    .tree-box {
+      margin-top: 25px;
+    }
+    .custom-tree-node {
+      flex: 1;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 14px;
+      padding-right: 8px;
+    }
   }
   .right-box {
     margin-left: 16px;
     width: 66%;
     background: #ffffff;
     overflow: auto;
+    .add-question {
+      padding: 0 24px;
+      height: 76px;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      border-bottom: 1px solid #ebeef5;
+      .question-btn {
+        width: 120px;
+        height: 40px;
+      }
+    }
+    .title-box {
+      padding: 26px 18px 0;
+      .title {
+        color: #333335;
+        font-size: 16px;
+      }
+      .record {
+        color: #333335;
+      }
+    }
+    .questions-box {
+      height: 400px;
+      overflow: auto;
+    }
+    .question-item {
+      margin: 22px 18px 0;
+      background: rgba(246, 247, 249, 1);
+      border-radius: 2px;
+      padding: 16px;
+      .question {
+        display: flex;
+        justify-content: space-between;
+        .question-icon {
+          color: rgba(25, 137, 250, 1);
+          font-size: 15px;
+          line-height: 15px;
+        }
+        .content {
+          margin-left: 8px;
+          font-weight: 500;
+        }
+        .edit {
+          color: #1989fa;
+        }
+        .delete {
+          color: #1989fa;
+          margin-left: 16px;
+        }
+      }
+      .answer-box {
+        margin: 13px 0 0 23px;
+        color: #3a3a3c;
+      }
+      .info-box {
+        color: #606266;
+        margin: 24px 0 0 23px;
+        .time {
+          margin-left: 44px;
+        }
+      }
+    }
+    .no-knowledge {
+      margin-top: 66px;
+      text-align: center;
+      .img {
+        width: 269px;
+        height: 195px;
+      }
+      .tip {
+        margin-top: 21px;
+        font-size: 18px;
+        color: #7e7f83;
+      }
+    }
   }
+}
+.crud-pagination {
+  background: #fff;
+  padding: 16px;
+  text-align: right;
+  margin-top: 25px;
 }
 </style>
