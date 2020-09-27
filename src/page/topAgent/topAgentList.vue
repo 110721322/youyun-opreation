ccecd4723d38fac6e405db526e94b38c85df1326
元芳 <1107213221@qq.com>
2020-09-27 17:17:04 +0800

    后台跳转

--- src/page/topAgent/topAgentList.vue
+++ src/page/topAgent/topAgentList.vue
@@ -204,7 +204,7 @@ export default {
         password: row.password
       }).then(res => {
         if (res.status === 0) {
-          window.open(process.env.VUE_APP_AGENTURL + 'ticket' + '=' + res.object)
+          window.location.href = process.env.VUE_APP_AGENTURL + 'ticket' + '=' + res.object
         }
       })
     },
