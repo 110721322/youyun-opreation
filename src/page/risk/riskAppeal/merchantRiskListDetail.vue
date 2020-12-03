dfda3648bfc37029bf1b4670ab6f4a2a29724c5d
元芳 <1107213221@qq.com>
2020-12-03 18:25:37 +0800

    cut: 无用模块删除

--- src/page/risk/riskAppeal/merchantRiskListDetail.vue
+++ src/page/risk/riskAppeal/merchantRiskListDetail.vue
@@ -68,7 +68,6 @@ import refuseImg from "@/assets/img/refuse.png";
 import approvalImg from "@/assets/img/approval.png";
 import detailMode from "@/components/detailMode/detailMode2.vue";
 import Form from "@/components/form/index.vue";
-import { CONFIG_DATA } from "../tableConfig/merchantRiskListDetail";
 import { FORM_CONFIG } from "../formConfig/leRiskListDetailConfig";
 
 export default {
@@ -91,7 +90,97 @@ export default {
       // pass通过 preApproval预审核 checking审核中 reject驳回
       currentType: "",
       ruleForm: {},
-      configData: CONFIG_DATA
+      configData: {
+        baseData: {
+          name: "基本信息",
+          items: [
+            {
+              name: "商户名称",
+              key: "merchantName"
+            },
+            {
+              name: "营业执照编号",
+              key: "shopLicenseNo"
+            },
+            {
+              name: "法人姓名",
+              key: "lawPerson"
+            },
+
+            {
+              name: "法人手机号",
+              key: "lawMobile"
+            },
+            {
+              name: "法人身份证号",
+              key: "idCardNo"
+            },
+            {
+              name: '银行卡号',
+              key: 'bankCardNo'
+            },
+            {
+              name: '商户地址',
+              key: 'merchantAddress'
+            }
+          ]
+        },
+        appealData: {
+          name: "商户信息",
+          items: [
+            {
+              name: "法人手持营业执照",
+              key: "shopLicenseImg",
+              type: "image"
+            },
+            {
+              name: "法人手持身份证正面",
+              key: "idCardHandImg",
+              type: "image"
+            },
+            {
+              name: "法人身份证原件正面",
+              key: "idCardPortraitImg",
+              type: "image"
+            },
+            {
+              name: "手持银行卡原件正面",
+              key: "bankCardImg",
+              type: "image"
+            },
+            {
+              name: "商户门店照片",
+              key: "shopFaceImg",
+              type: "image"
+            },
+            {
+              name: "其他照片",
+              key: "otherImg",
+              type: "image"
+            },
+            {
+              name: "商户机打指定小票照片1",
+              key: "machineTicketImg1",
+              type: "image"
+            },
+            {
+              name: "商户机打指定小票照片2",
+              key: "machineTicketImg2",
+              type: "image"
+            },
+            {
+              name: "商户机打指定小票照片3",
+              key: "machineTicketImg3",
+              type: "image"
+            },
+            {
+              name: "视频",
+              key: "video",
+              type: "video"
+            }
+          ]
+        }
+      }
     };
   },
   watch: {
