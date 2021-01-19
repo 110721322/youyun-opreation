a555be40dccdf5e4b98d4335c0a045e2f706b3e4
yunshi <17858802279@163.com>
2021-01-19 18:15:57 +0800

    fix:图片上传接口

--- src/api/api_common.js
+++ src/api/api_common.js
@@ -6,5 +6,9 @@ export default {
   // 发送忘记密码的验证码
   sendForgetPasswordCode: (params) => API.GET('/common/v1/user/sendForgetPasswordCode', params),
   // 修改密码
-  updatePassword: (params) => API.POST('/common/v1/user/updatePassword', params)
+  updatePassword: (params) => API.POST('/common/v1/user/updatePassword', params),
+  // oss获取图片上传Token
+  queryOssImgToken: () => API.POST('/common/v1/oss/queryOssToken', {type: 'common'}, {needFormData: true}),
+  // oss获取表格上传Token
+  queryOssExlToken: () => API.POST('/common/v1/oss/queryOssToken', {type: 'excel'}, {needFormData: true})
 };
