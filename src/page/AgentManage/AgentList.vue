c9c4757b0a365a7c1cfd44033169e1464635c034
admin <1107213221@qq.com>
2021-01-28 09:36:27 +0800

    to: 服务商跳转

--- src/page/AgentManage/AgentList.vue
+++ src/page/AgentManage/AgentList.vue
@@ -158,7 +158,7 @@
           password: row.password
         }).then(res => {
           if (res.status === 0) {
-            window.open(process.env.VUE_APP_AGENTURL + '#/login?ticket' + '=' + res.data)
+            window.open(process.env.AGENT_URL + '#/login?ticket' + '=' + res.data)
           }
         })
       },
