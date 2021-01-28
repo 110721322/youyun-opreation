a935e79c6e985621cb1d919d22e99075083f06a2
admin <1107213221@qq.com>
2021-01-28 10:47:16 +0800

    to: 跳转登录功能

--- src/page/AgentManage/AgentList.vue
+++ src/page/AgentManage/AgentList.vue
@@ -158,7 +158,7 @@
           password: row.password
         }).then(res => {
           if (res.status === 0) {
-            window.open('http://service.intranet.aduer.com/'+ '#/login?ticket' + '=' + res.data)
+            window.open('http://service.intranet.aduer.com/'+ '#/Login?ticket' + '=' + res.data)
             // window.open(process.env.AGENT_URL + '#/login?ticket' + '=' + res.data)
           }
         })
